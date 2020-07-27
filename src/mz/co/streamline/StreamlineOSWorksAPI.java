e4d30faf6961556739f4ac71f304e4ad7d8c9bb2
edmilson.cassecasse <edmilson.cassecasse@TC-LAP-44.mshome.net>
2020-07-27 18:38:51 +0200

    - Creating Persistence Annotations - Constructors, Getters, Setters, Equals and HashCode using Lombok - Validation Constraints using Hibernate Validator - Data Persistence using Hibernate Core - Creating a factory of instances - Adding the @valid annotation on collections

--- src/mz/co/streamline/StreamlineOSWorksAPI.java
+++ src/mz/co/streamline/StreamlineOSWorksAPI.java
@@ -5,11 +5,20 @@
  */
 package mz.co.streamline;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Set;
 import javax.persistence.Persistence;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import mz.co.streamline.osworks.core.connection.ConnectionFactoryBuilder;
 import mz.co.streamline.osworks.domain.model.Client;
 import mz.co.streamline.osworks.domain.model.Gender;
 import mz.co.streamline.osworks.domain.model.Physical;
+import org.hibernate.validator.messageinterpolation.ResourceBundleMessageInterpolator;
+import org.hibernate.validator.resourceloading.PlatformResourceBundleLocator;
 
 /**
  *
@@ -23,14 +32,35 @@ public class StreamlineOSWorksAPI {
     public static void main(String[] args) {
         // TODO code application logic here
         Physical physical = new Physical();
-        physical.setName("Edmilson de Azevedo");
+        physical.setName("Ed");
         physical.setSurname("Cassecasse");
         physical.setId(1L);
         physical.setGender(Gender.M);
         physical.setIdentificationNo("110204986669N");
-        //physical.setVatNumber(150155551);
+        physical.setVatNumber(150155551);
         
-        System.out.println(physical);
+        showMessages(physical);
+        
+    }
+    
+    private static void showMessages(Object object) {
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        Validator validator = Validation.byDefaultProvider()
+                .configure()
+                .messageInterpolator(new ResourceBundleMessageInterpolator(new PlatformResourceBundleLocator("Messages_pt_PT")))
+                .buildValidatorFactory().getValidator();
+        
+        String lang = "pt";
+        String country = "PT";
+        Locale locale = new Locale(lang, country);
+      
+        Set<ConstraintViolation<Object>> constraintViolations = validator.validate(object);
+        
+        if (constraintViolations.size() > 0) {
+            constraintViolations.forEach((constraint) -> 
+                    System.out.println(constraint.getMessage())
+            );
+        }
     }
     
 }
